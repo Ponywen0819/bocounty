0106bbbbe83d3e11dbf138751a92dc8ef2bc101a
Pony <pony076152340@gmail.com>
2023-09-19 12:53:25 +0800

    Admin report API

--- app/api/admin/report/route.py
+++ app/api/admin/report/route.py
@@ -2,7 +2,10 @@ from flask import Blueprint
 from app.utils.auth.auth_util import required_login
 from app.utils.response import success
 
-from .util.get import get_report_list
+from .util.validate import (
+    validate_report_exist
+)
+from .util.get import get_report_list, get_report
 
 report_api = Blueprint("report_admin_api", __name__, url_prefix='/report')
 
@@ -10,9 +13,20 @@ report_api = Blueprint("report_admin_api", __name__, url_prefix='/report')
 @report_api.route("/", methods=["GET"])
 @required_login(required_admin=True)
 def get_report_list_api():
-
     report_list = get_report_list()
 
     return success({
         "data": report_list
+    })
+
+
+@report_api.route("/<string:report_id>", methods=["GET"])
+@required_login(required_admin=True)
+def get_report_api(report_id: str):
+    validate_report_exist(report_id)
+
+    report = get_report(report_id)
+
+    return success({
+        "data": report
     })
--- app/api/admin/report/util/get.py
+++ app/api/admin/report/util/get.py
@@ -6,3 +6,11 @@ def get_report_list():
     report_list = get('report')
 
     return [Report(**data) for data in report_list]
+
+
+def get_report(report_id: str):
+    report = get('report', {
+        "id": report_id
+    })[0]
+
+    return Report(**report)
